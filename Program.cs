fbff25901db2404145ce4d57570e7ce25b2a7878
kiranravindrant <kiranravindrant@gmail.com>
2021-07-07 14:04:51 +0530

    [kiran]Added Project files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Program!");
+            //UC1_CalculateLength.CalculateLength();
+            //UC2_EqualityOfLines.CalculateLength();
+            UC3_CompareTwoLines.CalculateLength();
+        }
+    }
+        
+}
